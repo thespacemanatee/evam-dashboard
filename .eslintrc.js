ab756f6bd783e5ed994e3f908b83673973576af7
Chee Kit <cheekit.chong98@gmail.com>
2021-05-20 16:32:44 +0800

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': 'error',
+    'no-use-before-define': ['error', { variables: false }],
+    'react/jsx-filename-extension': [0, { extensions: ['.js', '.jsx'] }],
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+};
