efe02eb9ada929d174c31b280ff416088de2372f
Chee Kit <cheekit.chong98@gmail.com>
2021-06-27 16:52:21 +0800

    Added back button to temperature screen

--- src/screens/TemperatureScreen.tsx
+++ src/screens/TemperatureScreen.tsx
@@ -3,6 +3,7 @@ import { ImageBackground, StyleSheet, View } from 'react-native';
 
 import { FINAL_BASE_GRAPHIC_HEIGHT } from '../utils/config';
 import BaseGraphic from '../../assets/base-graphic.png';
+import ThemedIconButton from '../components/ThemedIconButton';
 
 const styles = StyleSheet.create({
   screen: {
@@ -15,12 +16,23 @@ const styles = StyleSheet.create({
     width: '101.75%',
     height: FINAL_BASE_GRAPHIC_HEIGHT * 1.1,
   },
+  buttonContainer: {
+    position: 'absolute',
+    left: 32,
+    top: 0,
+  },
 });
 
-const TemperatureScreen = () => {
+const TemperatureScreen = ({ navigation }) => {
   return (
     <View style={styles.screen}>
       <ImageBackground source={BaseGraphic} style={styles.baseGraphic} />
+      <View style={styles.buttonContainer}>
+        <ThemedIconButton
+          onPress={() => navigation.goBack()}
+          iconName='chevron-back-outline'
+        />
+      </View>
     </View>
   );
 };
