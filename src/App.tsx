9e056be5852b153dc057164f7cf56f3f4089e810
Chee Kit <cheekit.chong98@gmail.com>
2021-06-27 16:33:10 +0800

    Added splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { store } from './app/store';
 import AppNavigator from './navigation/AppNavigator';
 
-const App = (): React.ReactElement => {
-  const [loaded] = Font.useFonts({
-    // eslint-disable-next-line global-require
-    'Gotham-Narrow': require('../assets/fonts/Gotham-Narrow-Book.otf'),
-    // eslint-disable-next-line global-require
-    'Digital-Numbers': require('../assets/fonts/Digital-Numbers.ttf'),
+const App = (): React.ReactNode => {
+  const [loaded, setLoaded] = useState(false);
+
+  const loadAssets = async () => {
+    await SplashScreen.preventAutoHideAsync();
+    Font.loadAsync({
+      // eslint-disable-next-line global-require
+      'Gotham-Narrow': require('../assets/fonts/Gotham-Narrow-Book.otf'),
+      // eslint-disable-next-line global-require
+      'Digital-Numbers': require('../assets/fonts/Digital-Numbers.ttf'),
+    });
+  };
+
+  useEffect(() => {
+    loadAssets().then(() => {
+      setLoaded(true);
+      SplashScreen.hideAsync();
+    });
   });
 
   if (!loaded) {
-    return <></>;
+    return null;
   }
   return (
     <Provider store={store}>
