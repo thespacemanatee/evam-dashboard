3854ac97a7f8ef9f085e01662293fb875339d1eb
Chee Kit <cheekit.chong98@gmail.com>
2021-09-11 21:08:58 +0800

    Add CMake configuration files

---
+++ arduino/cmake-build-debug/CMakeFiles/Z_DUMMY_TARGET.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/thespacemanatee/Documents/GitHub/evam-dashboard/arduino/lib/Seeed_Arduino_CAN-master/src/can-serial.cpp" "/Users/thespacemanatee/Documents/GitHub/evam-dashboard/arduino/cmake-build-debug/CMakeFiles/Z_DUMMY_TARGET.dir/lib/Seeed_Arduino_CAN-master/src/can-serial.cpp.obj"
+  "/Users/thespacemanatee/Documents/GitHub/evam-dashboard/arduino/lib/Seeed_Arduino_CAN-master/src/mcp2515_can.cpp" "/Users/thespacemanatee/Documents/GitHub/evam-dashboard/arduino/cmake-build-debug/CMakeFiles/Z_DUMMY_TARGET.dir/lib/Seeed_Arduino_CAN-master/src/mcp2515_can.cpp.obj"
+  "/Users/thespacemanatee/Documents/GitHub/evam-dashboard/arduino/lib/Seeed_Arduino_CAN-master/src/mcp2518fd_can.cpp" "/Users/thespacemanatee/Documents/GitHub/evam-dashboard/arduino/cmake-build-debug/CMakeFiles/Z_DUMMY_TARGET.dir/lib/Seeed_Arduino_CAN-master/src/mcp2518fd_can.cpp.obj"
+  "/Users/thespacemanatee/Documents/GitHub/evam-dashboard/arduino/lib/Seeed_Arduino_CAN-master/src/mcp_can.cpp" "/Users/thespacemanatee/Documents/GitHub/evam-dashboard/arduino/cmake-build-debug/CMakeFiles/Z_DUMMY_TARGET.dir/lib/Seeed_Arduino_CAN-master/src/mcp_can.cpp.obj"
+  "/Users/thespacemanatee/Documents/GitHub/evam-dashboard/arduino/src/EVAM_CAN.cpp" "/Users/thespacemanatee/Documents/GitHub/evam-dashboard/arduino/cmake-build-debug/CMakeFiles/Z_DUMMY_TARGET.dir/src/EVAM_CAN.cpp.obj"
+  "/Users/thespacemanatee/Documents/GitHub/evam-dashboard/arduino/src/main.cpp" "/Users/thespacemanatee/Documents/GitHub/evam-dashboard/arduino/cmake-build-debug/CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
